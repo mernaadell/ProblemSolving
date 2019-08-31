5ddae40ca89e36c6f84d51a7239d321b9c2ac7fb
MernaAdel <31679900+mernaadell@users.noreply.github.com>
2019-08-30 21:51:58 -0700

    Add files via upload

---
+++ B-Drink.py
@@ -0,0 +1,32 @@
+if __name__ == "__main__":
+
+    t=int(input())
+    while(t):
+
+        pbf=input()
+        hc = input()
+        x=pbf.split()
+        y=hc.split()
+        p=int(x[0])
+        b=int(x[1])
+        f=int(x[2])
+        h=int(y[0])
+        c=int(y[1])
+        equ=(f*2+b*2)
+        if equ <= p:
+            print(h*b+c*f)
+        else:
+            i=p//2
+            if i*h >=i*c:
+                print(i*h)
+
+            else:
+                print(i*c)
+        t-=1
+
+
+
+
+
+
+
