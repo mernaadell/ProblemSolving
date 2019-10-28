af04e15fd0e649d95d7a13679542ce7f065065a4
mernaadell <mernaadel963@gmail.com>
2019-10-28 14:01:41 +0200

    solve -->https://www.hackerrank.com/challenges/sparse-arrays/problem

---
+++ sparseArray.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+# Complete the matchingStrings function below.
+if __name__ == '__main__':
+    x=int(input())
+    l=[input() for i in range(x)]
+    print(l) 
+    y=int(input())
+    l2=[input() for j in range(y)]
+    print(l2) 
+    answer=[]
+    for k2 in l2:
+        print(k2)
+        answer.append(l.count(k2))
+    for t in answer:
+        print(t)
