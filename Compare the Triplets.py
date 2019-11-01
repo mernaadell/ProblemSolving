5c2389c94bce30967231e0de947788073315249d
mernaadell <mernaadel963@gmail.com>
2019-11-01 04:49:01 +0200

    solve-->https://www.hackerrank.com/challenges/compare-the-triplets/problem

---
+++ Compare the Triplets.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+if __name__ == '__main__':
+    alice=0
+    bob=0
+    l =input().split()
+    l2 = input().split()
+    for i,j in zip(l,l2):
+        print(i,j)
+        if(i>j):
+            alice=alice+1
+        elif(i<j):
+            bob=bob+1
+    print(alice,bob)
+        
+        
+        
---
+++ comp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+# Complete the matchingStrings function below.
+if __name__ == '__main__':
+    alice=0
+    bob=0
+    a = list(map(int, input().rstrip().split()))
+    print(type(a[0]))
+    b = list(map(int, input().rstrip().split()))
+#
