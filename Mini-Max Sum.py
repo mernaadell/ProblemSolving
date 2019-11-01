b1f064ac1654a8b2e74833747f4259c8c9b202ba
mernaadell <mernaadel963@gmail.com>
2019-11-01 18:58:01 +0200

    solve-->https://www.hackerrank.com/challenges/mini-max-sum/problem

---
+++ Mini-Max Sum.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+    arr=sorted(arr)
+    print(sum(arr)-arr[4],sum(arr)-arr[0])
+   
