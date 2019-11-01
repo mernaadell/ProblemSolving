e3eec06e05532355bf02ce179962846733bc86dd
mernaadell <mernaadel963@gmail.com>
2019-11-01 05:05:48 +0200

    solve-->https://www.hackerrank.com/challenges/a-very-big-sum/problem

---
+++ A Very Big Sum.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+if __name__ == '__main__':
+    
+    x=int(input())
+    l =list(map(int,input().split()))
+    print(sum(l))
+        
+        
+        
