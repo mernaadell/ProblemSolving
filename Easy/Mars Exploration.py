b975fe254ec0ddd5eb19f677fb26783cb3f09723
mernaadell <mernaadel963@gmail.com>
2019-11-05 23:23:21 +0200

    solve-->https://www.hackerrank.com/challenges/mars-exploration/problem

---
+++ Easy/Mars Exploration.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 22:09:15 2019
+
+@author: merna
+"""
+
+if __name__ == '__main__':
+    count=0
+    s = input()
+    x=len(s)/3
+    s2="SOS"
+    j=0
+    for i in range(len(s)):
+        
+        if j==3:
+            j=0
+        if s[i]!=s2[j] :
+            count+=1
+            j+=1
+        else:
+            j+=1
+    print(count)
+            
+            
+ 
